76ded1b8491fcc5e6a9927350a1994d38638939a
natdev <dordonne.nathanael@gmail.com>
2025-12-15 00:34:07 -0400

    change url origin

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS messages(
 const app = express();
 const env = process.env.NODE_ENV || 'development';
 const httpServer = createServer(app);
-const url = env === 'development' ? 'http://localhost:4200' : 'https://your-production-domain.com';
+const url = env === 'development' ? 'http://localhost:4200' : 'https://chat-app-2vhb.onrender.com';
 const io = new Server(httpServer, {
   cors: {
     origin: url,
